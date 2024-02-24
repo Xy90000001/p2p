2566a8886db0bc248a21db9c2f0040db5f600999
Xy90000001 <106469364+Xy90000001@users.noreply.github.com>
2024-02-24 18:12:14 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
 import subprocess as s
 #import os as s
-s.run("/bin/bash peer2fly.sh --email xy90000001@gmail.com --number 5", shell=True)
+s.run("sudo su")
+s.run("./peer2fly.sh --email xy90000001@gmail.com --number 5", shell=True)
