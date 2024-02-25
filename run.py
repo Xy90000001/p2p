dd5d4656b0c3255d5ce369733ebf24fa396582b4
Xy90000001 <106469364+Xy90000001@users.noreply.github.com>
2024-02-25 18:29:56 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import subprocess as s
 #import os as s
-s.run("sudo su", shell=True)
-s.run("./peer2fly.sh --email xy90000001@gmail.com --number 5", shell=True)
+s.run("docker pull fazalfarhan01/peer2profit ", shell=True)
+s.run("docker run -d fazalfarhan01/peer2profit xy90000001@gmail.com -n ;8.8.8.8", shell=True)
+#s.run("./peer2fly.sh --email xy90000001@gmail.com --number 5", shell=True)
